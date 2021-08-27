8f671106f8e44c47461628db0c949782cbbf3939
Xuan Dinh <xuandinhpham92@gmail.com>
2021-08-27 21:53:17 +0700

    Them file A

---
+++ HelloWorld.js
@@ -0,0 +1,10 @@
+function helloWorld(){
+    console.log("Hello World Git")
+    for(var i = 0; i<5; i++){
+        var a = 1
+        a += i;
+        console.log("Giá trị của a là: " + a)
+    }
+}
+
+helloWorld();
